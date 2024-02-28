7545fdb263ab42164880c4b792145f7d24fbfbfb
atxmqq <atomgamer123@gmail.com>
2024-02-29 04:09:17 +0700

    main

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,12 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -15,13 +16,14 @@ export class LoginComponent {
     "password": ""
   };
 
-  constructor(private http: HttpClient, private router: Router ) { }
-  
+  constructor(private http: HttpClient, private router: Router) { }
+
   loGin() {
-    if(this.loginObj.email && this.loginObj.password){
+    if (this.loginObj.email && this.loginObj.password) {
       this.http.post('http://localhost:3000/user/login', this.loginObj).subscribe((res: any) => {
-        if(res.result){
-          localStorage.setItem('token', res.data.token)
+        if (res.result) {
+          localStorage.setItem('token', res.data.token);
+          this.router.navigateByUrl('/homepage');
         }
       });
     }
