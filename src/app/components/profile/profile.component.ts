b30f569f22b055ef5bf8025fcd0d9c88f1b5f8c7
atxmqq <atomgamer123@gmail.com>
2024-03-26 02:37:32 +0700

    q

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -8,13 +8,16 @@ import { CommonModule } from '@angular/common';
 import { UserGetRespon } from '../../../../model/UserGetRespon';
 import { ImageGetRespon } from '../../../../model/ImageGetRespon';
 import { MatIconModule } from '@angular/material/icon';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [RouterLink, MatButtonModule, MatDividerModule, HttpClientModule, CommonModule, MatIconModule],
+  imports: [RouterLink, MatButtonModule, MatDividerModule, HttpClientModule, CommonModule, MatIconModule, FormsModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
@@ -26,6 +29,17 @@ export class ProfileComponent implements AfterViewInit {
 
   imageDatabyPID: ImageGetRespon[] = [];
   pid: any;
+
+  DatabyUID: UserGetRespon[] = [];
+  editedUsername: any;
+  editedEmail: any;
+  userdataedit: UserGetRespon[] = [];
+  username: any;
+  email: any;
+  usernameedit: any;
+  passwordedit: any;
+  confirmpasswordedit: any;
+
 
   constructor(private http: HttpClient, private router: Router) { }
 
@@ -108,6 +122,30 @@ export class ProfileComponent implements AfterViewInit {
   }
 
 
+  openPopupProfileEdit(uid: any): void {
+    let popupProfileEdit = document.getElementById("popupProfileEdit");
+    if (popupProfileEdit) {
+      popupProfileEdit.classList.add("open-popup");
+    }
+
+    const url = `http://localhost:3000/user/${uid}`;
+    this.http.get<any>(url).subscribe((userData: any) => {
+      if (userData) {
+        console.log(userData);
+      } else {
+        console.log('No user data found');
+      }
+    });
+  }
+
+
+  closePopupProfileEdit() {
+    let popupProfileEdit = document.getElementById("popupProfileEdit");
+    if (popupProfileEdit) {
+      popupProfileEdit.classList.remove("open-popup");
+    }
+  }
+
 
   file?: File;
 
@@ -127,6 +165,21 @@ export class ProfileComponent implements AfterViewInit {
   }
 
   onFileSelectedChange(event: Event) {
+    if ((event.target as HTMLInputElement).files) {
+      this.file = (event.target as HTMLInputElement).files![0];
+
+      // ตรวจสอบว่า this.file ไม่เป็น null หรือ undefined ก่อนที่จะใช้งาน FileReader
+      if (this.file) {
+        const reader = new FileReader();
+        reader.onload = (e: any) => {
+          this.fileUrl = e.target.result;
+        };
+        reader.readAsDataURL(this.file);
+      }
+    }
+  }
+
+  onFileSelectedChangeProfile(event: Event) {
     if ((event.target as HTMLInputElement).files) {
       this.file = (event.target as HTMLInputElement).files![0];
 
@@ -258,4 +311,85 @@ export class ProfileComponent implements AfterViewInit {
         console.error('Error uploading image:', error);
       });
   }
+
+
+  confirmDelete(pid: any): void {
+    const result = window.confirm('Are you sure you want to delete this picture?');
+    if (result) {
+      this.deletePicture(pid);
+    }
+  }
+
+  deletePicture(pid: any): void {
+    this.http.delete<any>(`https://backend-projectanidex.onrender.com/upload/deleteimg/${pid}`)
+      .toPromise()
+      .then((response) => {
+        console.log('Delete successfully');
+        location.reload();
+      })
+      .catch((error) => {
+        console.log('Delete ERRORR!!!!');
+      });
+  }
+
+
+  editprofile(uid: any): void {
+    if (this.passwordedit !== this.confirmpasswordedit) {
+      alert('Password and ConfirmPassword not Match!!')
+      return;
+    }
+
+
+    const updatedUserData = {
+      username: this.usernameedit,
+      password: this.passwordedit
+    };
+
+    this.http.put<any>(`http://localhost:3000/user/editProfile/${uid}`, updatedUserData)
+      .toPromise()
+      .then((response) => {
+        console.log('Update successful');
+
+        this.usernameedit = '';
+        this.passwordedit = '';
+        this.confirmpasswordedit = '';
+
+        if (!this.file) {
+          console.log('No file selected.');
+          return;
+        }
+
+        const formData = new FormData();
+        formData.append('file', this.file);
+
+        this.http.post<any>('https://backend-projectanidex.onrender.com/upload', formData)
+          .toPromise()
+          .then((response) => {
+            console.log('Image uploaded. Firebase URL:', response.file);
+
+            // ส่งข้อมูลไปยัง Express Route เพื่อเพิ่มข้อมูลลงใน MySQL
+            const uploadData = {
+              imguser: response.file,
+            };
+
+            return this.http.put<any>(`http://localhost:3000/upload/uploadUserProfile/${uid}`, uploadData)
+              .toPromise();
+          })
+          .then(() => {
+            console.log('Data added to MySQL successfully.');
+          })
+          .catch((error) => {
+            console.error('Error uploading image:', error);
+          });
+
+
+        this.router.navigate(['/login']);
+
+
+      })
+      .catch((error) => {
+        console.log('Update failed');
+      });
+  }
+
 }
